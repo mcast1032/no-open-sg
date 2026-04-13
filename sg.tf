9c1e8f0a20b6d1149e6c2c12e36feeec37dbdee2
mcast1032 <mcast1032@gmail.com>
2026-04-13 19:48:03 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -13,7 +13,7 @@ resource "null_resource" "example" {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["10.0.0.0/16"]
       }]
     })
   }
